6cfb8bf3cd1edebe2f60eade952a81f64c728fdf
Mauzek <artemilliy@gmail.com>
2024-05-18 10:47:36 +0300

    1

---
+++ app/api/api-hooks.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import { getNormalizedGamesDataByCategory, isResponseOk } from './api-utils'
+
+export const useGetDataByCategory = (endpoint, category) => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getNormalizedGamesDataByCategory(endpoint, category)
+      if (isResponseOk(data)) {
+        setData(data)
+      }
+    }
+    fetchData()
+  }, [endpoint, category])
+
+  return data
+}
